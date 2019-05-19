d8e2d367c82d1974bfc81e9f216a6926aadf91bc
Alper Dursun <48094346+BayAlper10@users.noreply.github.com>
2019-05-19 14:29:28 +0300

    Add files via upload

--- src/BayAlper10/MoneyBE/Main.php
+++ src/BayAlper10/MoneyBE/Main.php
@@ -5,6 +5,8 @@ namespace BayAlper10\MoneyBE;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 use pocketmine\Server;
+use pocketmine\Player;
+use BayAlper10\MoneyBE\Main;
 
 use BayAlper10\MoneyBE\events\PlayerJoinEvent;
 
@@ -47,5 +49,41 @@ class Main extends PluginBase{
     $command->register("seemoney", new SeeMoney($this));
     $command->register("sendmoney", new SendMoney($this));
     $command->register("editmoney", new EditMoney($this));
+  }
+
+  public static function myMoney(Player $sender){
+    $sender->sendMessage("Paran: §6" . (string)Main::getInstance()->money->get($sender->getName()));
+  }
+
+  public static function sendMoney(Player $sender, Player $target, int $amount){
+    $sendername = $sender->getName();
+    if((int)$this->money->get($sendername) >= $amount){
+      $targetname = $target->getName();
+      Main::getInstance()->set($targetname, (int)Main::getInstance()->get($targetname) +  int($amount));
+      Main::getInstance()->money->set($sendername, (int)Main::getInstance()->money->get($sendername) - int($amount));
+      Main::getInstance()->money->save();
+
+      $sender->sendMessage($targetname . " adlı oyuncuya §7" . $amount . " §fpara gönderdin.");
+      $target->sendMessage($sendername . " adlı oyuncu size §7" . $amount . " §fpara gönderdi.");
+    }else{
+      $sender->sendMessage("§cGöndermek istediğin miktarda para bulunamadı.");
+    }
+  }
+
+  public static function editMoney(Player $sender, Player $target, int $amount){
+    $sendername = $sender->getName();
+    $targetname = $target->getName();
+
+    Main::getInstance()->set($targetname, (int)$amount);
+    Main::getInstance()->save();
+
+    $sender->sendMessage($targetname . " adlı oyuncunun parası §7" . $amount . " §folarak ayarlandı.");
+    $target->sendMessage("Bir yetkili paranızı §7" . $amount . " §folarak ayarladı.");
+  }
+
+  public static function seeMoney(Player $sender, Player $target){
+    $targetname = $target->getName();
+    $money = Main::getInstance()->get($targetname);
+    $sender->sendMessage($targetname . " adlı oyuncunun parası §7" . (int)$money);
   }
 }
