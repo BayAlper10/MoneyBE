c363c4c5b6bd3fbd1ae17925d582d9215b9f12f3
Alper Dursun <48094346+BayAlper10@users.noreply.github.com>
2019-05-19 14:28:02 +0300

    Add files via upload

--- src/BayAlper10/MoneyBE/economy/Economy.php
+++ src/BayAlper10/MoneyBE/economy/Economy.php
@@ -21,40 +21,4 @@ class Economy{
       return;
     }
   }
-
-  public static function myMoney(Player $sender){
-    $sender->sendMessage("Paran: §6" . (string)Main::getInstance()->money->get($sender->getName()));
-  }
-
-  public static function sendMoney(Player $sender, Player $target, int $amount){
-    $sendername = $sender->getName();
-    if((int)Main::getInstance()->money->get($sendername) >= $amount){
-      $targetname = $target->getName();
-      Main::getInstance()->money->set($targetname, (int)Main::getInstance()->money->get($targetname) +  int($amount));
-      Main::getInstance()->money->set($sendername, (int)Main::getInstance()->money->get($sendername) - int($amount));
-      Main::getInstance()->money->save();
-
-      $sender->sendMessage($targetname . " adlı oyuncuya §7" . $amount . " §fpara gönderdin.");
-      $target->sendMessage($sendername . " adlı oyuncu size §7" . $amount . " §fpara gönderdi.");
-    }else{
-      $sender->sendMessage("§cGöndermek istediğin miktarda para bulunamadı.");
-    }
-  }
-
-  public static function editMoney(Player $sender, Player $target, int $amount){
-    $sendername = $sender->getName();
-    $targetname = $target->getName();
-
-    Main::getInstance()->money->set($targetname, (int)$amount);
-    Main::getInstance()->money->save();
-
-    $sender->sendMessage($targetname . " adlı oyuncunun parası §7" . $amount . " §folarak ayarlandı.");
-    $target->sendMessage("Bir yetkili paranızı §7" . $amount . " §folarak ayarladı.");
-  }
-
-  public static function seeMoney(Player $sender, Player $target){
-    $targetname = $target->getName();
-    $money = Main::getInstance()->money->get($targetname);
-    $sender->sendMessage($targetname . " adlı oyuncunun parası §7" . (int)$money);
-  }
 }
